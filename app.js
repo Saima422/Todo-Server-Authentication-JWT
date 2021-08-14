7a400f64e13ef10db93c96f98099268e55a20fad
saima <sayedssaima04@gmail.com>
2021-08-15 04:36:49 +0530

    added an HTML

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const express = require("express");
 const dotenv = require("dotenv");
 const taskRouter = require("./routes/taskRouter");
 dotenv.config({ path: "./config.env" });
+// const serveStatic = require('serve-static');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// express.static(root);
+
+
+// app.use(serveStatic('public', { 'index': ['index.html'] }))
+app.use(express.static('public'))
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -15,7 +21,13 @@ app.use((req, res, next) => {
     next();
 });
 
+var showdown  = require('showdown'),
+    converter = new showdown.Converter(),
+    text      = '# hello, markdown!',
+    html      = converter.makeHtml(text);
+
 app.use("/tasks",taskRouter);
+
 
 // app.listen(3000, ()=>{
 //     console.log("Server started at port 3000");
