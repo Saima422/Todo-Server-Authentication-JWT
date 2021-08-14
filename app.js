9e579ef9eb668b1f35db7ff5b1319e5f316d4f28
saima <sayedssaima04@gmail.com>
2021-08-15 03:37:33 +0530

    added env port

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
+const path = require("path");
 const express = require("express");
+const dotenv = require("dotenv");
 const taskRouter = require("./routes/taskRouter");
-
+dotenv.config({ path: "./config.env" });
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 });
 
 app.use("/tasks",taskRouter);
 
-app.listen(3000, ()=>{
-    console.log("Server started at port 3000");
-});
+// app.listen(3000, ()=>{
+//     console.log("Server started at port 3000");
+// });
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log(`Server Started on port ${process.env.PORT || "3000"} `);
+})
