3f5593b480ade377acdc5494d0c7b7e275ab5bc3
saima <sayedssaima04@gmail.com>
2021-08-15 17:08:52 +0530

    attaching readme as frontpage

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const fs = require("fs");
 const express = require("express");
 const dotenv = require("dotenv");
 const taskRouter = require("./routes/taskRouter");
@@ -11,14 +12,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // var showdown  = require('showdown'),
-//     converter = new showdown.Converter(),
-//     text      = '# hello, markdown!',
+// converter = new showdown.Converter(),
+// text      = fs.readFileSync("./README.md").toString(),
 //     html      = converter.makeHtml(text);
 
-// app.use(serveStatic('public', `${html}`));
+let text= `<embed type="text/markdown" src="https://saima422.github.io/mardown-trial/" height="100%" width="100%"/>`;
+
+fs.writeFileSync("./public/index.html", text);
 
 // app.use(serveStatic('public', { 'index': ['index.html'] }))
-// app.use(express.static('public'))
+
+app.use(express.static('public'));
+
+// app.use(express.static());
+
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
